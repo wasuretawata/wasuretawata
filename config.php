51623aaf71f28b6b0aa4c16fab5797b609d1d92e
wasuretawata <wasuretawata@Demos-MacBook-Pro.local>
2022-06-26 15:32:12 +0800

    first commit

---
+++ config.php
@@ -0,0 +1,37 @@
+# Dependencies
+markdown: kramdown
+highlighter: rouge
+future: true
+
+plugins:
+    - jekyll-sitemap
+
+# File rules
+exclude: [vendor, Dangerfile, Gemfile, Gemfile.lock, LICENSE, README.md, CODE_OF_CONDUCT.md, CONTRIBUTING.md, new_draft.sh]
+
+# Permalinks
+permalink: /:title/
+
+# Site
+title: 'WebAssembly Weekly'
+description: 'A community-driven weekly newsletter about WebAssembly'
+baseurl: https://wasmweekly.news
+logo: /img/logo.png
+
+twitter:
+    username: WasmWeekly
+
+curator:
+    name: 'Colin Eberhardt'
+    url: https://twitter.com/ColinEberhardt
+
+# Setup
+excerpt_separator: <!--excerpt-->
+
+# Data
+links:
+    rss: '/feed.xml'
+    twitter: 'https://twitter.com/WasmWeekly'
+    github: 'https://github.com/ColinEberhardt/WasmWeekly'
+    issue: 'https://github.com/ColinEberhardt/WasmWeekly/issues/new'
+    pull: 'https://github.com/ColinEberhardt/WasmWeekly/compare'
